11dc440fda56e629cff62fa8994a602bb8f2ded2
nathan <xNathan0708@gmail.com>
2024-06-12 18:18:06 +0800

    tttt

--- src/components/TelegramUserInfo.jsx
+++ src/components/TelegramUserInfo.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 
 const TelegramUserInfo = () => {
-  const [user, setUser] = useState(null);
   const [initData, setInitData] = useState(null);
 
   useEffect(() => {
@@ -9,45 +8,34 @@ const TelegramUserInfo = () => {
     tg.ready();
 
     const initDataRaw = tg.initData;
-    const user = tg.initDataUnsafe?.user;
-
-    setInitData(parseInitData(initDataRaw));
-
-    if (user) {
-      setUser(user);
-    } else {
-      setUser({ first_name: "User", last_name: "Not found", username: "" });
-    }
+    const parsedInitData = parseInitData(initDataRaw);
+    setInitData(parsedInitData);
   }, []);
 
   const parseInitData = (data) => {
     const params = new URLSearchParams(data);
     const initDataObj = {};
     for (const [key, value] of params.entries()) {
-      initDataObj[key] = value;
+      try {
+        initDataObj[key] = JSON.parse(value);
+      } catch (e) {
+        initDataObj[key] = value;
+      }
     }
     return initDataObj;
   };
 
   return (
     <div>
-      {user ? (
-        <div>
-          <h2>User Information</h2>
-          <pre>
-            <code>{JSON.stringify(user, null, 2)}</code>
-          </pre>
-        </div>
-      ) : (
-        <p>Loading user info...</p>
-      )}
-      {initData && (
+      {initData ? (
         <div>
           <h2>Init Data</h2>
           <pre>
             <code>{JSON.stringify(initData, null, 2)}</code>
           </pre>
         </div>
+      ) : (
+        <p>Loading init data...</p>
       )}
     </div>
   );
