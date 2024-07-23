bcbfd454cefd5499f0868febfc1d288a3e76c819
nathan <xNathan0708@gmail.com>
2024-07-23 22:52:02 +0800

    fix:

--- src/App.jsx
+++ src/App.jsx
@@ -4,49 +4,49 @@ import useTelegram from "@/hooks/useTelegram";
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRoutes from "@/router";
 let init_data =
-  "query_id=AAFcoWJ6AAAAAFyhYnqqu9Sv&user=%7B%22id%22%3A2053284188%2C%22first_name%22%3A%22x%22%2C%22last_name%22%3A%22nathan%22%2C%22username%22%3A%22nathan2345%22%2C%22language_code%22%3A%22zh-hans%22%2C%22allows_write_to_pm%22%3Atrue%7D&auth_date=1719370655&hash=7994c2936a9bf6f38263daaa4f03c23817d129534fdce7914223dd57529dfe05";
+  "query_id=AAFgGzg6AwAAAGAbODoG8mvn&user=%7B%22id%22%3A7419206496%2C%22first_name%22%3A%22dfv%22%2C%22last_name%22%3A%22hnn%22%2C%22username%22%3A%22aptest1111%22%2C%22language_code%22%3A%22zh-hans%22%2C%22allows_write_to_pm%22%3Atrue%7D&auth_date=1721742911&hash=2e0dcb7632b53b532a9f943f8a887f342bf37e400412eb4444f0f8dabef25a1a";
 // eslint-disable-next-line react/prop-types
 const App = ({ initialInviteCode }) => {
   const { isTma, initData } = useTelegram();
   const [token, setToken] = useState(false);
 
-  useEffect(() => {
-    console.log("isTma", isTma, initData);
-    (async () => {
-      if (isTma && initData) {
-        // await $post("users/auth", { init_data: init_data })
-        await $post("users/auth", { init_data: initData })
-          .then(async (response) => {
-            console.log("users/auth", response);
-            localStorage.setItem("token", response.data.token);
-            await setToken(response.data.token);
-          })
-          .catch((error) => {
-            console.error("Error initializing user:", error);
-          });
-        await $post("users/invitation_code", { code: initialInviteCode })
-          .then((response) => {
-            console.log("users/invitation_code--------", response);
-          })
-          .catch((error) => {
-            console.error("Error initializing user:", error);
-          });
-      }
-    })();
-  }, [isTma, initData]);
-
   // useEffect(() => {
-  //   localStorage.removeItem("token");
-  //   $post("users/auth", { init_data: init_data })
-  //     .then((response) => {
-  //       console.log("users/auth", response);
-  //       localStorage.setItem("token", response.data.token);
-  //       setToken(response.data.token);
-  //     })
-  //     .catch((error) => {
-  //       console.error("Error initializing user:", error);
-  //     });
-  // }, []);
+  //   console.log("isTma", isTma, initData);
+  //   (async () => {
+  //     if (isTma && initData) {
+  //       // await $post("users/auth", { init_data: init_data })
+  //       await $post("users/auth", { init_data: initData })
+  //         .then(async (response) => {
+  //           console.log("users/auth", response);
+  //           localStorage.setItem("token", response.data.token);
+  //           await setToken(response.data.token);
+  //         })
+  //         .catch((error) => {
+  //           console.error("Error initializing user:", error);
+  //         });
+  //       await $post("users/invitation_code", { code: initialInviteCode })
+  //         .then((response) => {
+  //           console.log("users/invitation_code--------", response);
+  //         })
+  //         .catch((error) => {
+  //           console.error("Error initializing user:", error);
+  //         });
+  //     }
+  //   })();
+  // }, [isTma, initData]);
+
+  useEffect(() => {
+    localStorage.removeItem("token");
+    $post("users/auth", { init_data: init_data })
+      .then((response) => {
+        console.log("users/auth", response);
+        localStorage.setItem("token", response.data.token);
+        setToken(response.data.token);
+      })
+      .catch((error) => {
+        console.error("Error initializing user:", error);
+      });
+  }, []);
 
   // return <div className="App">{token && <Home token={token}></Home>}</div>;
   return (
