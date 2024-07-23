dd23ee31aabfb1bf330bfbba064d036c962505f3
nathan <xNathan0708@gmail.com>
2024-07-23 23:35:42 +0800

    fix: del proxy

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 // 创建一个 Axios 实例
 const api = axios.create({
-  // baseURL: "https://orca-summary-oryx.ngrok-free.app/v1/",
-  baseURL: "/v1",
+  baseURL: "https://orca-summary-oryx.ngrok-free.app/v1/",
+  // baseURL: "/v1",
   timeout: 10000,
   headers: {
     // "ngrok-skip-browser-warning": true,
