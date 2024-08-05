b997b17cfeac91151d01c2a959ba08149556a283
nathan <xNathan0708@gmail.com>
2024-08-06 00:19:23 +0800

    download tg

--- src/main.jsx
+++ src/main.jsx
@@ -2,20 +2,27 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "@/App.jsx";
 import "@/index.css";
-import WebApp from "@twa-dev/sdk";
+// import WebApp from "@twa-dev/sdk";
+// import WebApp from "./utils/tg-webapp";
+import "./utils/tg-webapp";
+
 // import VConsole from "vconsole"; // 引入 vConsole
 import { InviteCodeProvider } from "@/hooks/InviteCodeContext";
 
-WebApp.ready();
+// WebApp.ready();
+window.Telegram.WebApp.ready(() => {
+  console.log("WebApp is ready!");
+});
 
 // new VConsole();
 console.clear();
-console.log("WebApp.initDataUnsafe.start_param------------", WebApp.initDataUnsafe.start_param);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <InviteCodeProvider>
-      <App initialInviteCode={WebApp.initDataUnsafe.start_param} />
+      <App
+        initialInviteCode={window.Telegram.WebApp.initDataUnsafe.start_param}
+      />
     </InviteCodeProvider>
   </React.StrictMode>
 );
